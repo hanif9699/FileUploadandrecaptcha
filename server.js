7c61e7304948a1fe69f569997ea9170ddc68f1c6
hanif9699 <hanif9699bad@gmail.com>
2020-02-09 17:33:01 +0530

    File Upload code

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express=require('express');
 const bodyparser=require('body-parser');
 const request=require('request');
+const fs=require('fs')
 
 const app=express();
 
-app.use(bodyparser.urlencoded({extended:false}));
-app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false,limit: '50mb'}));
+app.use(bodyparser.json({limit: '50mb'}));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 })
 
 app.post('/send',(req,res)=>{
-    console.log(req.body.captcha)
+    /* console.log(req.body.captcha) */
     if(req.body['captcha'] === undefined || req.body['captcha'] === '' || req.body['captcha'] === null)
   {
     return res.json({"responseError" : "Please select captcha first"});
   }
+  const file=req.body['file'];
+  const filedata=file.data;
+  const filename=file.name;
+  fs.writeFile(filename, filedata, 'base64', (err) => {
+    console.log(err);
+});
   const secretKey = "6LeaWcoUAAAAALOXkpzsJQEvyR3VR79UoxNPpO4a";
   // console.log(req.connection.remoteAddress)
   const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['captcha'] + "&remoteip=" + req.connection.remoteAddress;
@@ -28,6 +35,7 @@ app.post('/send',(req,res)=>{
     if(body.success !== undefined && !body.success) {
       return res.json({"responseError" : "Failed captcha verification"});
     }
+    
     res.json({"responseSuccess" : "Sucess"});
     // console.log(res)
   });
